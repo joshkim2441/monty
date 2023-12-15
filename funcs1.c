35119ecb4199f4d906d5d0e1acde8d8a669148c8
joshkim2441 <joshkim2441@gmail.com>
2023-12-15 10:00:32 +0300

    Monty

--- funcs1.c
+++ funcs1.c
@@ -6,24 +6,25 @@
  * @line_number: the line number
  * Return: none
 */
-void mt_rotl(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+void mt_rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack, *curr;
+	stack_t *tmp1, *tmp2 = NULL;
+	(void)line_number;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		return;
 	}
-	curr = (*stack)->next;
-	curr->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *stack;
+	tmp1 = (*stack)->next;
+	tmp2 = *stack;
+	for (; tmp2->next != NULL; tmp2 = tmp2->next)
+		;
+
+	tmp1->prev = NULL;
+	tmp2->next = *stack;
 	(*stack)->next = NULL;
-	(*stack)->prev = tmp;
-	(*stack) = curr;
+	(*stack)->prev = tmp2;
+	*stack = tmp1;
 }
 
 /**
@@ -34,22 +35,24 @@ void mt_rotl(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 */
 void mt_rotr(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
-	stack_t *tp;
+	stack_t *tmp = NULL;
+	(void)line_number;
 
-	tp = *stack;
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		return;
 	}
-	while (tp->next)
-	{
-		tp = tp->next;
-	}
-	tp->next = *stack;
-	tp->prev->next = NULL;
-	tp->prev = NULL;
-	(*stack)->prev = tp;
-	(*stack) = tp;
+
+	tmp = *stack;
+
+	for (; tmp->next != NULL; tmp = tmp->next)
+		;
+
+	tmp->prev->next = NULL;
+	tmp->next = *stack;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	*stack = tmp;
 }
 
 /**
@@ -62,7 +65,7 @@ void mt_queue(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-	montglb.fili = 1;
+	montglb.fili = 0;
 }
 
 /**
@@ -98,4 +101,18 @@ void que_add(stack_t **stack, int n)
 		curr->next = n_node;
 		n_node->prev = curr;
 	}
+}
+
+/**
+ * mt_stack - function to set data format to stack
+ * @stack: the linked list head
+ * @line_number: the opcode line number
+ * Return: none
+*/
+void mt_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	montglb.fili;
 }
